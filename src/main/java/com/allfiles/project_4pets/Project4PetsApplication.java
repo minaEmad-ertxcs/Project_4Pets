cecb58d327da635d594e5e39ab9f05cd95a51c20
minaEmad-ertxcs <85185270+minaEmad-ertxcs@users.noreply.github.com>
2022-05-08 00:27:24 +0200

    Start the project

---
+++ src/main/java/com/allfiles/project_4pets/Project4PetsApplication.java
@@ -0,0 +1,13 @@
+package com.allfiles.project_4pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4PetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project4PetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allfiles/project_4pets/Project4PetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allfiles.project_4pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4PetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
